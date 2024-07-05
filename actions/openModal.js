8c716b5ff54ca5c6fec7bfeeb23dbad8e16f69ed
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-05 19:07:05 +0200

    Update openModal.js

--- actions/openModal.js
+++ actions/openModal.js
@@ -1,15 +1,17 @@
+import "./imgModal.css";
+
 export const openModal = (src) => {
 
   const modal = document.createElement("div");
-  modal.classList.add("modal");
+  modal.className = "modal";
   document.body.appendChild(modal);
 
   const modalImg = document.createElement("img");
   modalImg.src = src;
-  modalImg.classList.add("modal-img");
+  modalImg.className = "modal-img";
   modal.appendChild(modalImg);
 
-  modal.addEventListener('click', () => {
+  modal.addEventListener("click", () => {
     document.body.removeChild(modal);
   });
 }

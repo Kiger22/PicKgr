63c9178b361126b3372013d17cb5f5bcd8a60bf7
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-05 15:24:19 +0200

    Update OnLogin.js

--- actions/OnLogin.js
+++ actions/OnLogin.js
@@ -1,7 +1,6 @@
-import { callRandomApi } from "../main";
-import { createloginSection, openLoginSection } from "../src/components/Formularios/inicioSeccion"
+import { createloginSection } from "../src/components/Formularios/inicioSeccion"
 
 export const OnLogin = () => {
-  openLoginSection();
+  createloginSection();
 
 }
